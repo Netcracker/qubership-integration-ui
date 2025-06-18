07c97c9f0d2237821b8f5db5a8f1b387508824e3
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2025-06-18 09:43:20 +0300

    fix: fixed super-linter configuration

--- .github/linters/eslint.config.js
+++ .github/linters/eslint.config.js
@@ -1,18 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact, { rules } from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
       "react/react-in-jsx-scope": "off",
     },
   },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
