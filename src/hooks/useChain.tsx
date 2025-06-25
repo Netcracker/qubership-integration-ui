7622105bb7b8df36f1c7d60b514a540d27ca9182
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2025-06-25 15:07:23 +0300

    fix: fixed useChain hooks

--- src/hooks/useChain.tsx
+++ src/hooks/useChain.tsx
@@ -1,33 +1,43 @@
 import { api } from "../api/api.ts";
 import { Chain } from "../api/apiTypes.ts";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useNotificationService } from "./useNotificationService.tsx";
 
 export const useChain = (chainId?: string) => {
+  const [isLoading, setIsLoading] = useState(false);
   const [chain, setChain] = useState<Chain>();
   const notificationService = useNotificationService();
 
-  useEffect(() => {
-    getChain().then(setChain);
-  }, []);
+  const updateChain = useCallback(
+    async (chain: Partial<Chain>) => {
+      if (!chainId) return;
+      setIsLoading(true);
+      try {
+        return api.updateChain(chainId, chain);
+      } catch (error) {
+        notificationService.requestFailed("Failed to update chain", error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [chainId, notificationService],
+  );
 
-  const updateChain = async (chain: Partial<Chain>) => {
+  const getChain = useCallback(async () => {
     if (!chainId) return;
-    try {
-      return api.updateChain(chainId, chain);
-    } catch (error) {
-      notificationService.requestFailed("Failed to update chain", error);
-    }
-  };
-
-  const getChain = async () => {
-    if (!chainId) return;
+    setIsLoading(true);
     try {
       return api.getChain(chainId);
     } catch (error) {
       notificationService.requestFailed("Failed to load snapshots", error);
+    } finally {
+      setIsLoading(false);
     }
-  };
+  }, [chainId, notificationService]);
 
-  return { chain, setChain, updateChain };
+  useEffect(() => {
+    getChain().then(setChain);
+  }, [getChain]);
+
+  return { isLoading, chain, setChain, updateChain };
 };
