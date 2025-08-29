5472e1e47548cf2b91d9bb586ef7fade43a100f9
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2025-08-29 18:35:04 +0300

    feat: added tooltip to a connection anchor component

--- src/components/mapper/ConnectionAnchor.tsx
+++ src/components/mapper/ConnectionAnchor.tsx
@@ -1,7 +1,10 @@
 import React, { forwardRef, useEffect, useState } from "react";
 import styles from "./ConnectionAnchor.module.css";
+import { Tooltip, TooltipProps } from "antd";
 
 export type ConnectionAnchorProps = React.HTMLAttributes<HTMLElement> & {
+  tooltipTitle?: TooltipProps["title"];
+  tooltipPlacement?: TooltipProps["placement"];
   invalid?: boolean;
   connected: boolean;
   onClick?: (connected: boolean) => void;
@@ -10,32 +13,39 @@ export type ConnectionAnchorProps = React.HTMLAttributes<HTMLElement> & {
 export const ConnectionAnchor = forwardRef<
   HTMLDivElement,
   ConnectionAnchorProps
->(({ invalid, connected, onClick, ...props }, ref) => {
-  const [className, setClassName] = useState<string>(
-    `${styles["connection-anchor"]} ${styles["disconnected"]}`,
-  );
+>(
+  (
+    { invalid, connected, onClick, tooltipPlacement, tooltipTitle, ...props },
+    ref,
+  ) => {
+    const [className, setClassName] = useState<string>(
+      `${styles["connection-anchor"]} ${styles["disconnected"]}`,
+    );
 
-  useEffect(() => {
-    const classNames: (keyof typeof styles)[] = [
-      styles["connection-anchor"],
-      styles[connected ? "connected" : "disconnected"],
-    ];
-    if (invalid) {
-      classNames.push(styles["invalid"]);
-    }
-    setClassName(classNames.join(" "));
-  }, [invalid, connected]);
+    useEffect(() => {
+      const classNames: (keyof typeof styles)[] = [
+        styles["connection-anchor"],
+        styles[connected ? "connected" : "disconnected"],
+      ];
+      if (invalid) {
+        classNames.push(styles["invalid"]);
+      }
+      setClassName(classNames.join(" "));
+    }, [invalid, connected]);
 
-  return (
-    <div
-      ref={ref}
-      className={className}
-      {...props}
-      onClick={() => onClick?.(connected)}
-    >
-      <div className={styles["inner-circle"]}></div>
-    </div>
-  );
-});
+    return (
+      <Tooltip title={tooltipTitle ?? ""} placement={tooltipPlacement}>
+        <div
+          ref={ref}
+          className={className}
+          {...props}
+          onClick={() => onClick?.(connected)}
+        >
+          <div className={styles["inner-circle"]}></div>
+        </div>
+      </Tooltip>
+    );
+  },
+);
 
 ConnectionAnchor.displayName = "ConnectionAnchor";
