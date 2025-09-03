34879ebee55b99f26cc108662696a3aa0f6a4bd5
Altair Kabdrakhmanov <altair200294@gmail.com>
2025-09-03 11:46:21 +0500

    fix: vite.config.lib.ts and package.json (#113)

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -3,18 +3,38 @@ import react from "@vitejs/plugin-react";
 import sassDts from 'vite-plugin-sass-dts';
 import dts from "vite-plugin-dts";
 
+const external = [
+    'react',
+    'react-dom',
+    'antd',
+    'tailwindcss',
+    '@tanstack/react-query',
+    '@tanstack/router-devtools',
+    '@tanstack/router-plugin'
+];
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sassDts(), dts()],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'QIP-UI',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`,
+    plugins: [
+        react(),
+        sassDts(),
+        dts({
+            entryRoot: 'src',
+            outDir: 'dist-lib/types',
+            insertTypesEntry: true
+        }),
+    ],
+    build: {
+        outDir: 'dist-lib',
+        emptyOutDir: true,
+        lib: {
+            entry: 'src/index.ts',
+            name: 'QIP-UI',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            external
+        },
     },
-    rollupOptions: {
-      external: [],
-    }
-  }
 });
