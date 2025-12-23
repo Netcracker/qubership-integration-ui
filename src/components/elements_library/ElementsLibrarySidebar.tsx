56155985658e1af4889510d9bb5d37948ad3c04e
dmitriipisarev <dmitrii.pisarev.qubership@gmail.com>
2025-12-23 15:41:49 +0300

    feat: #146 Search bar for element library (#329)

--- src/components/elements_library/ElementsLibrarySidebar.tsx
+++ src/components/elements_library/ElementsLibrarySidebar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useCallback, useEffect, useRef, useState } from "react";
 import { Menu, Spin } from "antd";
 import { LibraryElement, LibraryData } from "../../api/apiTypes.ts";
 import DraggableElement from "./DraggableElement.tsx";
@@ -9,20 +9,29 @@ import { useNotificationService } from "../../hooks/useNotificationService.tsx";
 import { useLibraryContext } from "../LibraryContext.tsx";
 import { IconName, OverridableIcon } from "../../icons/IconProvider.tsx";
 import { getElementColor } from "../../misc/chain-graph-utils.ts";
+import { Flex } from "antd/lib/index";
+import { SidebarSearch } from "./SidebarSearch.tsx";
+
+export type MenuItem = {
+  key: string;
+  label: React.ReactNode;
+  name: string;
+  icon?: ReactElement;
+  children?: MenuItem[];
+};
 
 export const ElementsLibrarySidebar = () => {
   const [, setElementsList] = useState<LibraryData | null>(null);
+  const allItems = useRef<MenuItem[]>([]);
   const [items, setItems] = useState<MenuItem[]>([]);
   const [loading, setLoading] = useState(true);
   const notificationService = useNotificationService();
   const { libraryData, isLibraryLoading } = useLibraryContext();
 
-  type MenuItem = {
-    key: string;
-    label: React.ReactNode;
-    icon?: ReactElement;
-    children?: MenuItem[];
-  };
+  const [openKeysState, setOpenKeysState] = useState<string[]>();
+  const openKeysBeforeSearch = useRef<string[]>();
+  const [isSearch, setIsSearch] = useState(false);
+
 
   useEffect(() => {
     if (libraryData) {
@@ -34,9 +43,11 @@ export const ElementsLibrarySidebar = () => {
         group.elements.forEach((element: LibraryElement) => {
           if (element.deprecated || element.unsupported) return;
           if (!folderMap.has(element.folder)) {
+            const name = prettifyName(element.folder);
             folderMap.set(element.folder, {
               key: element.folder,
-              label: prettifyName(element.folder),
+              label: name,
+              name: name,
               icon: <OverridableIcon name="folderOpenFilled" style={{ fontSize: 18, color: getElementColor(element) }} />,
               children: [],
             });
@@ -46,6 +57,7 @@ export const ElementsLibrarySidebar = () => {
             const childMenuItem = {
               key: child.name,
               label: <DraggableElement element={libraryData.childElements[child.name]} />,
+              name: child.name,
               icon: <OverridableIcon name={child.name as IconName} style={{ fontSize: 18 }} />,
             };
             childrenMenuItems.push(childMenuItem);
@@ -53,6 +65,7 @@ export const ElementsLibrarySidebar = () => {
           const elementMenuItem: MenuItem = {
             key: element.name,
             label: <DraggableElement element={element} />,
+            name: element.title,
             icon: <OverridableIcon name={element.name as IconName} style={{ fontSize: 18 }} />,
           };
           if (childrenMenuItems.length !== 0) {
@@ -64,7 +77,7 @@ export const ElementsLibrarySidebar = () => {
         });
       });
 
-      const sortedFolders = Array.from(folderMap.values()).sort((a, b) =>
+      const sortedFolders: MenuItem[] = Array.from(folderMap.values()).sort((a, b) =>
         a.key.localeCompare(b.key)
       );
 
@@ -74,10 +87,24 @@ export const ElementsLibrarySidebar = () => {
         }
       });
 
-      setItems(sortedFolders);
+      allItems.current = sortedFolders;
+      setItems([...sortedFolders]);
       setLoading(false);
     }
   }, [libraryData, notificationService]);
+
+  const handleSearch = useCallback(
+    (filtered: MenuItem[], openKeys: string[]) => {
+      if (!isSearch) {
+        setIsSearch(true);
+        openKeysBeforeSearch.current = openKeysState;
+      }
+      setOpenKeysState(openKeys);
+      setItems(filtered);
+
+    },
+    [isSearch, openKeysState],
+  );
 
   const prettifyName = (name: string): string => {
     const result = name.replace(/-/g, " ");
@@ -93,13 +120,26 @@ export const ElementsLibrarySidebar = () => {
       {isLibraryLoading && loading ? (
         <Spin />
       ) : (
-        <Menu
-          className={styles.libraryElements}
-          mode="inline"
-          theme="light"
-          items={items}
-          inlineIndent={8}
-        />
+        <Flex vertical >
+          <SidebarSearch
+            items={allItems.current}
+            onSearch={handleSearch}
+            onClear={() => {
+              setItems(allItems.current);
+              setIsSearch(false);
+              setOpenKeysState(openKeysBeforeSearch.current);
+            }}
+          />
+          <Menu
+            className={styles.libraryElements}
+            mode="inline"
+            theme="light"
+            items={items}
+            inlineIndent={8}
+            openKeys={openKeysState}
+            onOpenChange={(keys) => setOpenKeysState(keys)}
+          />
+        </Flex>
       )}
     </Sider>
   );
