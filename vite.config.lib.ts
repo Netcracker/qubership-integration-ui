3da0481139fba7a6664a23f3ad2c3faef778bc4a
AkhDmitrii <fuzzzi2410@gmail.com>
2026-01-22 11:21:00 +0300

    Revert "fix: fix linter errors"
    This reverts commit 42b3cd8a59ab27771d4f9c241be0387db34143f3.

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,41 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sassDts from "vite-plugin-sass-dts";
+import sassDts from 'vite-plugin-sass-dts';
 import dts from "vite-plugin-dts";
 import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sassDts(),
-    dts({
-      entryRoot: "src",
-      outDir: "dist-lib/types",
-      insertTypesEntry: true,
-      rollupTypes: true,
-    }),
-  ],
-  build: {
-    outDir: "dist-lib",
-    emptyOutDir: true,
-    minify: true,
-    sourcemap: false,
-    rollupOptions: {
-      input: path.resolve(__dirname, "src/index.ts"),
-      external: ["react", "react-dom", "react/jsx-runtime"],
-      preserveEntrySignatures: "exports-only",
-      output: {
-        format: "es",
-        entryFileNames: "index.es.js",
-        inlineDynamicImports: true,
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name?.endsWith(".css")) {
-            return "qip-ui.css";
-          }
-          return "assets/[name]-[hash][extname]";
+    plugins: [
+        react(),
+        sassDts(),
+        dts({
+            entryRoot: 'src',
+            outDir: 'dist-lib/types',
+            insertTypesEntry: true,
+            rollupTypes: true,
+        })
+    ],
+    build: {
+        outDir: "dist-lib",
+        emptyOutDir: true,
+        minify: true,
+        sourcemap: false,
+        rollupOptions: {
+            input: path.resolve(__dirname, "src/index.ts"),
+            external: [
+                "react",
+                "react-dom",
+                "react/jsx-runtime"
+            ],
+            preserveEntrySignatures: "exports-only",
+            output: {
+                format: "es",
+                entryFileNames: "index.es.js",
+                inlineDynamicImports: true,
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name?.endsWith(".css")) {
+                        return "qip-ui.css";
+                    }
+                    return "assets/[name]-[hash][extname]";
+                },
+            }
         },
-      },
     },
-  },
 });
