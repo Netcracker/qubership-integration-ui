f2f8432a08614f20bad7c3c6ec4237b28d54b55c
AkhDmitrii <fuzzzi2410@gmail.com>
2026-01-23 18:26:31 +0300

    fix: add linter configs

--- .github/linters/eslint.config.mjs
+++ .github/linters/eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginJest from "eslint-plugin-jest";
 import * as reactHooks from "eslint-plugin-react-hooks";
@@ -20,7 +19,6 @@ export default defineConfig([
   ]),
 
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
   pluginReact.configs.flat.recommended,
   reactHooks.configs["recommended-latest"],
 
