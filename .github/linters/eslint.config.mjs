a27826f6bdf1a525cb73b7b14bed07ab60a95808
AkhDmitrii <fuzzzi2410@gmail.com>
2026-01-26 11:45:49 +0300

    fix: change linter configs

--- .github/linters/eslint.config.mjs
+++ .github/linters/eslint.config.mjs
@@ -1,13 +1,13 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginJest from "eslint-plugin-jest";
-import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import * as reactHooks from "eslint-plugin-react-hooks";
+import { globalIgnores } from "eslint/config";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig([
+/** @type {import("eslint").Linter.Config[]} */
+export default [
   globalIgnores([
     "dist/*",
     "node_modules/*",
@@ -15,31 +15,60 @@ export default defineConfig([
     "**/jest.config*.ts",
     "**/eslint.config*.{js,mjs}"
   ]),
-
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   pluginReact.configs.flat.recommended,
-
+  reactHooks.configs["recommended-latest"],
   {
-    files: ["**/*.{spec,test}.{js,ts,tsx}"],
-    plugins: { jest: pluginJest },
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     languageOptions: {
-      globals: pluginJest.environments.globals.globals
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.js"],
+          defaultProject: "./tsconfig.json",
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     rules: {
-      "jest/valid-expect": ["error", { maxArgs: 2 }],
-      "jest/no-disabled-tests": "warn",
-      "jest/no-focused-tests": "error",
-      "jest/no-identical-title": "error",
-      "jest/prefer-to-have-length": "warn"
-    }
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
-
+  {
+    // update this to match your test files
+    files: ['**/*.spec.js', '**/*.test.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      "jest/valid-expect": [
+        "error",
+        {
+          maxArgs: 2,
+        },
+      ],
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+    },
+  },
   {
     rules: {
       "n/no-missing-import": "off",
       "n/no-unsupported-features/node-builtins": "off",
       "react-hooks/exhaustive-deps": "off",
       "no-unused-vars": "off",
+      "no-undef": "off"
     }
   }
-]);
+];
