55db1ed390fadb410ebd4df89e8b80b78c56e35d
SukhochevAleksei <aleksei.sukhochev.qubership@gmail.com>
2026-02-12 14:09:50 +0300

    fix: Reduce duplicated values in operation list in service call parameters

--- src/misc/operations-utils.ts
+++ src/misc/operations-utils.ts
@@ -1,15 +1,17 @@
-import {SystemOperation} from "../api/apiTypes.ts";
+import { SystemOperation } from "../api/apiTypes.ts";
 
-export function uniqueMapById(items: SystemOperation[]): Map<string, SystemOperation> {
-    const m = new Map<string, SystemOperation>();
-    items.forEach((op) => {
-        if (!m.has(op.id)) {
-            m.set(op.id, op);
-        }
-    });
-    return m;
+export function uniqueMapById(
+  items: SystemOperation[],
+): Map<string, SystemOperation> {
+  const m = new Map<string, SystemOperation>();
+  items.forEach((op) => {
+    if (!m.has(op.id)) {
+      m.set(op.id, op);
+    }
+  });
+  return m;
 }
 
 export function uniqueListById(items: SystemOperation[]): SystemOperation[] {
-    return Array.from(uniqueMapById(items).values());
+  return Array.from(uniqueMapById(items).values());
 }
