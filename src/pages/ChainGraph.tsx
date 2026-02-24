27a63bbbf00cee52d04aec4149dfba5a2946b901
zhonniks <samenelya@gmail.com>
2026-02-24 19:21:29 +0500

    fix: right side panel element focus

--- src/pages/ChainGraph.tsx
+++ src/pages/ChainGraph.tsx
@@ -27,6 +27,11 @@ import { Flex, FloatButton, Modal } from "antd";
 import { useNavigate } from "react-router";
 import { useParams } from "react-router-dom";
 import { CustomControls } from "../components/graph/CustomControls.tsx";
+import {
+  ElementFocus,
+  ElementFocusContext,
+  type FitViewToElementIdFn,
+} from "../components/graph/ElementFocus.tsx";
 import FloatButtonGroup from "antd/lib/float-button/FloatButtonGroup";
 import { useModalsContext } from "../Modals.tsx";
 import { ChainElementModification } from "../components/modal/chain_element/ChainElementModification.tsx";
@@ -141,6 +146,7 @@ const ChainGraphInner: React.FC = () => {
   );
 
   const { rightPanel, toggleRightPanel } = useElkDirection();
+  const fitViewToElementIdRef = useRef<FitViewToElementIdFn | null>(null);
 
   const handleElementUpdated = useCallback(
     (element: Element, node: ChainGraphNode) => {
@@ -495,7 +501,8 @@ const ChainGraphInner: React.FC = () => {
             toggleRightPanel,
           }}
         >
-          <ReactFlow
+          <ElementFocusContext.Provider value={fitViewToElementIdRef}>
+            <ReactFlow
             nodes={nodes}
             nodeTypes={nodeTypes}
             defaultEdgeOptions={{ zIndex: 1001 }}
@@ -522,6 +529,7 @@ const ChainGraphInner: React.FC = () => {
             onPaneClick={closeMenu}
             fitView
           >
+            <ElementFocus />
             <Background variant={BackgroundVariant.Dots} />
             <MiniMap
               zoomable
@@ -534,9 +542,10 @@ const ChainGraphInner: React.FC = () => {
             <CustomControls />
             {menu && <ContextMenu menu={menu} closeMenu={closeMenu} />}
           </ReactFlow>
+          {rightPanel && <PageWithRightPanel />}
+        </ElementFocusContext.Provider>
         </ElkDirectionContextProvider>
       </div>
-      {rightPanel && <PageWithRightPanel />}
       <FloatButtonGroup trigger="hover" icon={<OverridableIcon name="more" />}>
         <FloatButton
           icon={<>⭾</>}
