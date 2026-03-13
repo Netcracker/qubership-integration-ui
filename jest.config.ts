7d483f9f15736480498df748f35d1fac123cad62
Ravikant <ravikant.ceg@gmail.com>
2026-03-13 10:19:10 +0530

    test(dev-tools): add NamespaceField tests with validation coverage
    - Add tests/setup/jest-dom.ts for reusable matchMedia mock
    - Add jest-dom setup to jest.config.ts
    - Add NamespaceField rendering and validation tests

--- jest.config.ts
+++ jest.config.ts
@@ -141,7 +141,10 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["jest-expect-message"],
+  setupFilesAfterEnv: [
+    "jest-expect-message",
+    "<rootDir>/tests/setup/jest-dom.ts",
+  ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
