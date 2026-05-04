ff07997dc71cb555cec69d0667be04e9cf87f341
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2026-05-04 10:58:19 +0300

    feat: added flags to control domain types availability

--- src/components/SelectDomains.tsx
+++ src/components/SelectDomains.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Select, Space, Tag } from "antd";
 import { useDomains } from "../hooks/useDomains.tsx";
 import { LabelInValueType } from "rc-select/lib/Select";
 import type { FlattenOptionData } from "rc-select/lib/interface";
 import { DomainType, EngineDomain } from "../api/apiTypes.ts";
+import { getConfig, onConfigChange } from "../appConfig.ts";
 
 export type Domain = {
   name: string;
@@ -15,10 +16,7 @@ export type SelectDomainsProperties = {
   onChange?: (value: Domain[]) => void;
 };
 
-function getDomainType(
-  domainId: string,
-  domains: EngineDomain[],
-): DomainType {
+function getDomainType(domainId: string, domains: EngineDomain[]): DomainType {
   return (
     domains.find((domain) => domainId === domain.id)?.type ?? DomainType.MICRO
   );
@@ -29,6 +27,15 @@ export const SelectDomains: React.FC<SelectDomainsProperties> = ({
   onChange,
 }) => {
   const { isLoading: isDomainsLoading, domains } = useDomains();
+  const [domainTypes, setDomainTypes] = useState<DomainType[]>([]);
+
+  useEffect(() => {
+    const updateDomainTypes = (cfg: ReturnType<typeof getConfig>) => {
+      setDomainTypes(cfg.domainTypes ?? [DomainType.CLASSIC, DomainType.MICRO]);
+    };
+    updateDomainTypes(getConfig());
+    return onConfigChange((config) => updateDomainTypes(config));
+  }, []);
 
   const renderOption = useCallback(
     (props: LabelInValueType | FlattenOptionData<unknown>) => {
@@ -47,18 +54,20 @@ export const SelectDomains: React.FC<SelectDomainsProperties> = ({
 
   return (
     <Select
-      value={value?.map(domain => domain.name)}
+      value={value?.map((domain) => domain.name)}
       loading={isDomainsLoading}
-      mode="tags"
+      mode={domainTypes.includes(DomainType.MICRO) ? "tags" : "multiple"}
       allowClear
       labelRender={renderOption}
       optionRender={renderOption}
       options={domains.map((domain) => ({
         value: domain.id,
-        label: domain.name
+        label: domain.name,
       }))}
       onChange={(values) => {
-        onChange?.(values.map(name =>({ name, type: getDomainType(name, domains )})));
+        onChange?.(
+          values.map((name) => ({ name, type: getDomainType(name, domains) })),
+        );
       }}
     ></Select>
   );
